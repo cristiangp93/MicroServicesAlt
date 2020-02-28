d911419df6d86a60fc693923605407c5c7c9e4c8
Cristian <cpguillenp80@est.ucacue.edu.ec>
2020-02-27 22:16:11 -0500

    github pages test

--- src/app/pages/servicio2/servicio2.component.ts
+++ src/app/pages/servicio2/servicio2.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import {S2Service} from '../../services/s2.service';
 
 @Component({
   selector: 'app-servicio2',
@@ -7,9 +9,107 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Servicio2Component implements OnInit {
 
-  constructor() { }
+  loading: boolean;
+  datos: any = [];
+  selectedCredit: any = [];
+  cuotas: any = [];
+  cloudDeploy = false;
+
+  constructor(public s2: S2Service) {
+    this.get_prestamos();
+  }
 
   ngOnInit(): void {
+  }
+
+  get_prestamos() {
+    this.loading = true;
+    if ( this.cloudDeploy ) {
+      console.log('cloud');
+      this.s2.get_creditos_cloud()
+        .subscribe((resp: any) => {
+          this.loading = false;
+          if ( resp.statusCode !== 400) {
+            this.datos = resp.body;
+          } else {
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: resp.body
+            });
+          }
+        });
+    } else {
+      console.log('server');
+      this.s2.get_creditos_server()
+        .subscribe((resp: any) => {
+          this.loading = false;
+          if ( resp.statusCode !== 400) {
+            this.datos = resp.body;
+          } else {
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: resp.body
+            });
+          }
+        });
+    }
+  }
+
+  pagar_cuota(cuotaIdx: number ) {
+    Swal.fire({
+      allowOutsideClick: false,
+      text: 'Espere por favor'
+    });
+    Swal.showLoading();
+    if ( this.cloudDeploy ) {
+      console.log('cloud');
+      this.s2.pagar_cuota_cloud({amort: this.selectedCredit, cuotaIdx})
+        .subscribe((resp: any) => {
+          Swal.close();
+          if ( resp.statusCode !== 400) {
+            Swal.fire({
+              icon: 'success',
+              title: 'Ok !',
+              text: resp.body
+            }).then(() => {
+              this.selectedCredit = [];
+              this.cuotas = [];
+              this.get_prestamos();
+            });
+          } else {
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: resp.body
+            });
+          }
+        });
+    } else {
+      console.log('server');
+      this.s2.pagar_cuota_server({amort: this.selectedCredit, cuotaIdx})
+        .subscribe((resp: any) => {
+          Swal.close();
+          if ( resp.statusCode !== 400) {
+            Swal.fire({
+              icon: 'success',
+              title: 'Ok !',
+              text: resp.body
+            }).then(() => {
+              this.selectedCredit = [];
+              this.cuotas = [];
+              this.get_prestamos();
+            });
+          } else {
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: resp.body
+            });
+          }
+        });
+    }
   }
 
 }
--- src/app/services/s2.service.ts
+++ src/app/services/s2.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class S2Service {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  get_creditos_cloud() {
+    return this.http.get(`${environment.awsURL}/creditos`);
+  }
+
+  get_creditos_server() {
+    return this.http.get(`${environment.apiURL}/api/creditos`);
+  }
+
+  pagar_cuota_cloud(payload: any) {
+    return this.http.put(`${environment.awsURL}/pagar`, payload);
+  }
+
+  pagar_cuota_server( payload: any) {
+    return this.http.put(`${environment.apiURL}/api/pagar`, payload);
+  }
 }
