b464c921095afb6da82ca7561b5a1b467a6d9419
Cristian <cpguillenp80@est.ucacue.edu.ec>
2020-05-05 20:30:33 -0500

    05-05-20 =)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  // apiURL: 'https://18.229.171.187:3000',
-  apiURL: 'https://localhost:3000',
-  // awsURL: 'https://pv6jaz89q6.execute-api.sa-east-1.amazonaws.com/dev'
+  apiURL: 'http://18.228.9.188:3000/',
   awsURL: 'https://19cw5hnspd.execute-api.sa-east-1.amazonaws.com/dev/'
 };
